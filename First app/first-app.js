2007325c6f3f910995b99af68eaa843e2e96d634
Mahmoud Sharshera <43113784+zixa5824@users.noreply.github.com>
2024-02-08 22:45:51 +0200

    First tutorial finished

--- First app/first-app.js
+++ First app/first-app.js
@@ -1,1 +1,4 @@
-console.log("Hello World!!!");
+//console.log("Hello World!!!")
+
+const fs = require('fs'); // will include file system inside a constant named fs
+fs.writeFileSync('Hello.txt', 'Hello from node.js'); // then we write inside a file
