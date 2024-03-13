e1580c1f33cb4d02a3e5480d9f70709d5e123fb8
Mahmoud Sharshera <43113784+zixa5824@users.noreply.github.com>
2024-03-13 02:02:27 +0200

    Server almost finished

--- First app/app.js
+++ First app/app.js
@@ -1,6 +1,4 @@
 const http = require('http');
-
-const server = http.createServer((req,res)=>{
-    console.log(req);
-});
+const routes = require('./routes');
+const server = http.createServer(routes);
 server.listen(3000);
