394c1d14a702da584829961d527cfed8d5132af0
Mahmoud Sharshera <43113784+zixa5824@users.noreply.github.com>
2024-03-14 23:29:08 +0200

    Assignment 1 DONE!

--- Assignment 1/routes.js
+++ Assignment 1/routes.js
@@ -1,5 +1,4 @@
-const fs = require('fs');
-const requestHandler = (res,req)=>{
+const requestHandler = (req,res)=>{
     const url = req.url;
     const method = req.method;
     if(url === '/')
@@ -7,9 +6,10 @@ const requestHandler = (res,req)=>{
         res.setHeader('Content_Type', 'text/html');
         res.write('<html>');
         res.write('<head><title>Greetings</title></head>');
-        res.write('<body><h1>GREETINGS!</h1></body>');
+        res.write('<body><h1>GREETINGS!</h1>');
+        res.write('<h2><form action="/create-users" method="POST"><input type="text" name="username"><button type="submit">Send</button></form></h2></body>')
         res.write('</html>');
-        res.end();
+        return res.end();
     }
     if(url === '/users')
     {
@@ -18,7 +18,22 @@ const requestHandler = (res,req)=>{
         res.write('<head><title>Users</title></head>');
         res.write('<body><ul><li>User 1</li><li>User 2</li><li>User 3</li></ul></body>');
         res.write('</html>');
-        res.end();
+        return res.end();
+    }
+    if(url === '/create-users' && method === 'POST')
+    {
+        const body = [];
+        req.on('data', (stream)=>body.push(stream));
+        return req.on('end', ()=>{
+            const parsedBody = Buffer.concat(body).toString();
+            console.log(parsedBody);
+            message = parsedBody.split('=')[1];
+            console.log(message);
+            res.statusCode = 302; // REDIRECTED !!
+            res.setHeader('Location','/');
+            return res.end();
+        });
+
     }
 }
-exports = requestHandler;
+module.exports = requestHandler;
