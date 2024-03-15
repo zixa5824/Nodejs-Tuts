b57d364b51a72a471f335cf70061683191905469
Mahmoud Sharshera <43113784+zixa5824@users.noreply.github.com>
2024-03-15 14:27:28 +0200

    Express first mini project

---
+++ Express app/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+app.use('/add-product',(req,res,next)=>{
+    console.log('In the another middleware!');
+    res.send('<h1>The add product page</h1>');
+});
+
+app.use('/',(req,res,next)=>{
+    console.log('In the another middleware!');
+    res.send('<h1>Hello From EXPRESS!</h1>');
+});
+
+app.listen(3000);
