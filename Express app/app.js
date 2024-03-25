ebd910bf50b73e2f91e8166c6e345269b77d3e56
Mahmoud Sharshera <43113784+zixa5824@users.noreply.github.com>
2024-03-26 00:22:51 +0200

    static folder done

--- Express app/app.js
+++ Express app/app.js
@@ -7,11 +7,12 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended:false}));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin',adminRoutes);
 app.use(shopRoutes);
 
 app.use((req,res,next)=>{
-    res.status(404).sendFile(path.join(__dirname, 'views', 'not-found.html'));
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(3000);
